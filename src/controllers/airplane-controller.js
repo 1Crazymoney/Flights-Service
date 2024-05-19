2f09367a3b89603f9e2a6ddb0d2bce89097a0e6b
Ashutosh Shukla <er.ashutoshcse@gmail.com>
2024-05-19 17:32:57 +0530

    Added delete airplane api

--- src/controllers/airplane-controller.js
+++ src/controllers/airplane-controller.js
@@ -72,8 +72,32 @@ async function getAirplane(req,res){
     }
 }
 
+/**
+ * 
+ * DELETE : /airplanes/:id
+ * req-body {}
+ */
+
+async function destroyAirplane(req,res){
+    try {
+        const airplanes = await AirplaneService.destroyAirplane(req.params.id);
+        SuccessResponse.data = airplanes;
+        return res
+            .status(StatusCodes.CREATED)
+            .json(SuccessResponse);
+        
+    } catch (error) {
+        ErrorResponse.error = error;
+        return res
+            .status(error.statusCode)
+            .json(ErrorResponse);
+        
+    }
+}
+
 module.exports = {
     createAirplane,
     getAirplanes,
-    getAirplane
+    getAirplane,
+    destroyAirplane
 }

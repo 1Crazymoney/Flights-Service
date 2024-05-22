84a8bb72788956d0300f83d3b2349a5975c4a660
Ashutosh Shukla <er.ashutoshcse@gmail.com>
2024-05-23 00:13:40 +0530

    FEAT: Added filters for get all flights

--- src/models/flight.js
+++ src/models/flight.js
@@ -12,51 +12,52 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
       this.belongsTo(models.Airplane, {
-        foreignKey:'airplaneId'
+        foreignKey: 'airplaneId'
       });
       this.belongsTo(models.Airport, {
-        foreignKey:'code'
+        foreignKey: 'departureAirportId'
       });
       this.belongsTo(models.Airport, {
-        foreignKey:'code'
+        foreignKey: 'arrivalAirportId'
       });
 
     }
   }
   Flight.init({
-    flightNumber:{ 
-      type:DataTypes.STRING,
-      allowNull:false
+    flightNumber: {
+      type: DataTypes.STRING,
+      allowNull: false
     },
     airplaneId: {
-      type:DataTypes.INTEGER,
-      allowNull:false
+      type: DataTypes.INTEGER,
+      allowNull: false
     },
     departureAirportId: {
-      type:DataTypes.STRING,
-      allowNull:false
+      type: DataTypes.STRING,
+      allowNull: false
     },
     arrivalAirportId: {
-      type:DataTypes.STRING,
-      allowNull:false
+      type: DataTypes.STRING,
+      allowNull: false
     },
     arrivalTime: {
-      type:DataTypes.DATE,
-      allowNull:false
+      type: DataTypes.DATE,
+      allowNull: false
     },
-    departureTime:{
-      type:DataTypes.DATE,
-      allowNull:false
+    departureTime: {
+      type: DataTypes.DATE,
+      allowNull: false
     },
     price: {
-      type:DataTypes.INTEGER,
-      allowNull:false
+      type: DataTypes.INTEGER,
+      allowNull: false
     },
     boardingGate: {
-      type:DataTypes.STRING},
+      type: DataTypes.STRING
+    },
     totalSeats: {
-      type:DataTypes.INTEGER,
-      allowNull:false
+      type: DataTypes.INTEGER,
+      allowNull: false
     }
   }, {
     sequelize,

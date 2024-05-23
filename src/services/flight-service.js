1aaf36cd9ce359f35550a9b7677c73bcf2350e43
Ashutosh Shukla <er.ashutoshcse@gmail.com>
2024-05-23 19:05:11 +0530

    Added complex join for fetching all airports

--- src/services/flight-service.js
+++ src/services/flight-service.js
@@ -1,5 +1,5 @@
 const { StatusCodes } = require('http-status-codes');
-const {Op} = require('sequelize');
+const { Op } = require('sequelize');
 
 const AppError = require('../Utils/errors/app-error');
 const { FlightRepository } = require('../repositories');
@@ -34,32 +34,33 @@ async function getAllFlights(query) {
         [departureAirportId, arrivalAirportId] = query.trips.split('-');
         customFilter.departureAirportId = departureAirportId;
         customFilter.arrivalAirportId = arrivalAirportId;
+
     }
-    if (query.price){
+    if (query.price) {
         [minPrice, maxPrice] = query.price.split('-');
         customFilter.price = {
-            [Op.between]: [minPrice,((maxPrice == undefined) ? 20000 : maxPrice)]
+            [Op.between]: [minPrice, ((maxPrice == undefined) ? 20000 : maxPrice)]
         }
     }
-    if (query.travellers){
+    if (query.travellers) {
         customFilter.totalSeats = {
             [Op.gte]: query.travellers
         }
     }
-    if(query.tripDate){
+    if (query.tripDate) {
         customFilter.departureTime = {
-            [Op.between]:[query.tripDate,query.tripDate + endingTripTime]
+            [Op.between]: [query.tripDate, query.tripDate + endingTripTime]
         }
     }
-    if(query.sort){
+    if (query.sort) {
         const params = query.sort.split(',');
-        const sortFilters = params.map((param)=>param.split('_'));
+        const sortFilters = params.map((param) => param.split('_'));
         sortFilter = sortFilters
 
 
     }
     try {
-        const flights = await flightRepository.getAllFlights(customFilter,sortFilter);
+        const flights = await flightRepository.getAllFlights(customFilter, sortFilter);
         return flights;
 
     } catch (error) {

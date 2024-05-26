6ac7113a6d9feefa7525e5bf26766e8aa8d47b5a
Ashutosh Shukla <er.ashutoshcse@gmail.com>
2024-05-26 16:04:55 +0530

    Added flight seats update api

--- src/repositories/flight-repository.js
+++ src/repositories/flight-repository.js
@@ -2,6 +2,8 @@ const CrudRepository = require('./crud-repository');
 const { Flight, Airplane, Airport,City } = require('../models')
 const { Sequelize, Op } = require('sequelize');
 
+const db = require('../models');
+
 class FlightRepository extends CrudRepository {
     constructor() {
         super(Flight);
@@ -44,6 +46,20 @@ class FlightRepository extends CrudRepository {
         });
         return response;
     }
+
+    async updateRemainingSeats(flightId,seats, dec=true){
+        await db.sequelize.query(`Select * from Flights WHERE Flights.id = ${flightId} FOR UPDATE;`);
+        const flight = await Flight.findByPk(flightId);
+        if (parseInt(dec)){
+            await flight.decrement('totalSeats',{by:seats});
+        } else {
+           await flight.increment('totalSeats',{by:seats});
+        }
+        return flight;
+    
+    }
+    
 }
+
 
 module.exports = FlightRepository;
--- src/routes/V1/flight-routes.js
+++ src/routes/V1/flight-routes.js
@@ -8,18 +8,24 @@ const router = express.Router();
 // /api/v1/flights POST
 router
     .post('/',
-        FlightMiddlewares.validateCreateRequest,
-        FlightController.createFlight);
+    FlightMiddlewares.validateCreateRequest,
+    FlightController.createFlight);
 
 // /api/v1/flights?trips=MUM-DEL GET
 router
     .get('/',
-        FlightController.getAllFlights);
+    FlightController.getAllFlights);
 
 // /api/v1/flights/:id GET
 
 router
-    .get('/',
-        FlightController.getFlight);
+    .get('/:id',
+    FlightController.getFlight);
 
+// /api/v1/flights/:id/seats PATCH
+
+router
+    .patch('/:id/seats',
+    FlightMiddlewares.validateUpdateSeatsRequest,
+    FlightController.updateSeats);
 module.exports = router; 
